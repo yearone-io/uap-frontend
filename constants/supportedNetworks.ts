d02c175b0c921b36a3dfe1605956be60abc856c0
democrazyfi <132473036+democrazyfi@users.noreply.github.com>
2024-12-16 18:50:06 +0400

    Update URD address

--- constants/supportedNetworks.ts
+++ constants/supportedNetworks.ts
@@ -40,7 +40,7 @@ export const supportedNetworks: { [key: string]: ChainInfo } = {
     ipfsGateway: 'https://api.universalprofile.cloud/ipfs',
     explorer: 'https://explorer.execution.testnet.lukso.network/',
     token: 'LYXt',
-    protocolAddress: '0x5B7799d1899E8756BfCe2369Fcd9fED53C0d6D7C',
+    protocolAddress: '0xBe5D7f0815561A130159196B02125e4B0073b1a1',
     defaultURDUP: '0x7870C5B8BC9572A8001C3f96f7ff59961B23500D',
     hasUPSupport: true,
     icon: '/lyx_icon_testnet.svg',

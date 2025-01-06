94e01aad962132cd263ea646b38f35122c76ef1c
Ledespana <no-reply@ledespana.com>
2025-01-06 16:59:50 +0100

    initial

--- app/[networkName]/catalog/screener-assistants/[assistantAddress]/configure/page.tsx
+++ app/[networkName]/catalog/screener-assistants/[assistantAddress]/configure/page.tsx
@@ -1,14 +1,79 @@
 'use client';
-import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { Box, Button, Flex, Text, VStack } from '@chakra-ui/react';
+import AssistantInfo from '@/components/AssistantInfo';
+import { forwarderAssistant } from '@/constants/dummyData';
+import URDSetup from '@/components/URDSetup';
+import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers/react';
+import SignInBox from '@/components/SignInBox';
+import { getNetwork } from '@/utils/utils';
+import { getChainIdByUrlName } from '@/utils/universalProfile';
+import { useNetwork } from '@/contexts/NetworkContext';
+import {
+  doesControllerHaveMissingPermissions,
+  isDelegateAlreadySet,
+} from '@/utils/configDataKeyValueStore';
+import { useProfile } from '@/contexts/ProfileContext';
+import SetupAssistant from '@/components/SetupAssistant';
 import Breadcrumbs from '@/components/Breadcrumbs';
 
-export default function ConfigureAssistantPage({
+export default function ScreenerAssistantConfigurePage({
   params,
 }: {
-  params: { assistantAddress: string; networkName: string };
+  params: { networkName: string; assistantAddress: string };
 }) {
   const { networkName } = params;
+  const networkUrlId = getChainIdByUrlName(params.networkName);
+  const { open } = useWeb3Modal();
+  const { mainUPController } = useProfile();
+  const [isMissingPermissions, setIsMissingPermissions] = React.useState(false);
+  const [isURDInstalled, setIsURDInstalled] = React.useState(false);
+  const { network } = useNetwork();
+
+  const { address, chainId: walletNetworkId } = useWeb3ModalAccount();
+  // todo validate that id from url is a valid assistant id
+
+  useEffect(() => {
+    console.log('mainUPController', mainUPController);
+
+    if (!address || !mainUPController) {
+      return;
+    }
+
+    const getMissingPermissions = async () => {
+      try {
+        const missingPermissions = await doesControllerHaveMissingPermissions(
+          mainUPController,
+          address
+        );
+        setIsMissingPermissions(missingPermissions.length > 0);
+      } catch (error) {
+        console.error('Error checking permissions', error);
+      }
+    };
+
+    const checkURDInstalled = async () => {
+      console.log('checkURDInstalled called');
+      try {
+        const urdInstalled = await isDelegateAlreadySet(
+          address,
+          network.protocolAddress
+        );
+        console.log('urdInstalled', urdInstalled);
+        setIsURDInstalled(urdInstalled);
+      } catch (error) {
+        console.error('Error checking assistant installation', error);
+      }
+    };
+
+    checkURDInstalled();
+    getMissingPermissions();
+  }, [
+    address,
+    mainUPController,
+    network.protocolAddress,
+    setIsMissingPermissions,
+  ]);
 
   const breadCrumbs = Breadcrumbs({
     items: [
@@ -19,7 +84,7 @@ export default function ConfigureAssistantPage({
         href: `/${networkName}/catalog`,
       },
       {
-        name: `Screener ${params.assistantAddress}`,
+        name: `Assistant ${params.assistantAddress}`,
         href: `/${networkName}/catalog/screener-assistants/${params.assistantAddress}`,
       },
       {
@@ -28,22 +93,55 @@ export default function ConfigureAssistantPage({
       },
     ],
   });
+  const renderConfigureBody = () => {
+    if (!walletNetworkId || !address) {
+      return <SignInBox boxText={'Sign in to set UAPTypeConfig'} />;
+    }
+
+    if (walletNetworkId !== networkUrlId) {
+      return (
+        <Flex
+          height="100%"
+          w="100%"
+          alignContent="center"
+          justifyContent="center"
+          pt={4}
+        >
+          <VStack>
+            <Text>You're connect to {getNetwork(walletNetworkId).name}.</Text>
+            <Text>Please change network</Text>
+            <Button onClick={() => open({ view: 'Networks' })}>
+              Change network
+            </Button>
+          </VStack>
+        </Flex>
+      );
+    }
+    console.log('renderConfigureBody');
+    console.log('isMissingPermissions', isMissingPermissions);
+    console.log('mainUPController', mainUPController);
+    console.log('isURDInstalled', isURDInstalled);
+    if (!mainUPController || isMissingPermissions || !isURDInstalled) {
+      // todo pass isMissingPermissions to URDSetup
+      return <URDSetup />;
+    }
+
+    return (
+      <SetupAssistant assistantAddress={params.assistantAddress as string} />
+    );
+  };
 
   return (
-    <>
+    <Box p={4} w="100%">
       {breadCrumbs}
-      <Flex
-        display="flex"
-        w={'100%'}
-        flexDirection={'column'}
-        flexWrap={'wrap'}
-        gap={4}
-        mt={4}
-      >
-        <Box flex="1" w={'100%'} maxWidth="800px">
-          CATALOG SCREENER ASSISTANT CONFIGURE PAGE
-        </Box>
+      <Flex direction="column" gap={4} mt={4} w="100%">
+        <Flex w="100%">
+          <AssistantInfo assistant={forwarderAssistant} />
+        </Flex>
+        <Box border="1px" borderColor="gray.200" w="100%" />
+        {renderConfigureBody()}
+        <Box border="1px" borderColor="gray.200" w="100%" />
       </Flex>
-    </>
+    </Box>
   );
 }

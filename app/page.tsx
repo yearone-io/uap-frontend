3e589bd71727185dc6762ceb5f744926bcac2b03
user.name <user@email>
2025-02-04 20:35:56 +0400

    permanent redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 // app/page.tsx
-import { redirect } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 import { CHAINS } from '@/constants/supportedNetworks';
 
 export default function HomePage() {
   // This will perform a server-side redirect to '/lukso'
-  redirect(`/${CHAINS.LUKSO_TESTNET}`);
+  permanentRedirect(`/${CHAINS.LUKSO_TESTNET}`);
   // The component returns nothing since redirect() never allows further rendering.
   return null;
 }

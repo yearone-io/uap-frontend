6703e4f4b07b06b83dc5322ae2d532f6aecb1257
tehnalogos <128236767+tehnalogos@users.noreply.github.com>
2025-03-03 11:29:33 +0200

    format issues (#71)
    Co-authored-by: user.name <user@email>

--- constants/assistantsConfig.ts
+++ constants/assistantsConfig.ts
@@ -44,13 +44,15 @@ export const tipAssistantTestnet: ExecutiveAssistant = {
 export const forwarderAssistantTestnet: ExecutiveAssistant = {
   address: '0x67cc9c63af02f743c413182379e0f41ed3807801',
   name: 'Forwarder Assistant',
-  description:
-    'Forward incoming assets to an external address.',
+  description: 'Forward incoming assets to an external address.',
   iconPath: '/assistants/forwarder.jpg',
   links: [{ name: 'X', url: 'https://x.com/yearone_io' }],
   assistantType: 'Executive',
   creatorAddress: '0xfE67D89DeBEC38592aB2FeD217b8bbb28851DF88',
-  supportedTransactionTypes: [LSP1_TYPE_IDS.LSP7Tokens_RecipientNotification, LSP1_TYPE_IDS.LSP8Tokens_RecipientNotification],
+  supportedTransactionTypes: [
+    LSP1_TYPE_IDS.LSP7Tokens_RecipientNotification,
+    LSP1_TYPE_IDS.LSP8Tokens_RecipientNotification,
+  ],
   configParams: [
     {
       name: 'targetAddress',
@@ -155,13 +157,15 @@ export const tipAssistantMainnet: ExecutiveAssistant = {
 export const forwarderAssistantMainnet: ExecutiveAssistant = {
   address: '0xf958412f67085b616ce741e7432ab6acac08b5d0',
   name: 'Forwarder Assistant',
-  description:
-    'Forward incoming assets to an external address.',
+  description: 'Forward incoming assets to an external address.',
   iconPath: '/assistants/forwarder.jpg',
   links: [{ name: 'X', url: 'https://x.com/yearone_io' }],
   assistantType: 'Executive',
   creatorAddress: '0xec1c59E78De6f840A66b6EE8E4066700Be863529',
-  supportedTransactionTypes: [LSP1_TYPE_IDS.LSP7Tokens_RecipientNotification, LSP1_TYPE_IDS.LSP8Tokens_RecipientNotification],
+  supportedTransactionTypes: [
+    LSP1_TYPE_IDS.LSP7Tokens_RecipientNotification,
+    LSP1_TYPE_IDS.LSP8Tokens_RecipientNotification,
+  ],
   configParams: [
     {
       name: 'targetAddress',
